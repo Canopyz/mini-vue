36cdb426bf7486bccf1a4529c88b03cbd12d8c4e
Canopyz <2575744741@qq.com>
2023-10-13 16:29:13 +0800

    实现初始化element, text的主流程

--- example/helloWorld/App.js
+++ example/helloWorld/App.js
@@ -4,5 +4,17 @@ export const App = {
   setup() {
     return { msg: 'Hello Vue 3!' }
   },
-  render: () => h('div', 'hello'),
+  render: () =>
+    h(
+      'div',
+      {
+        id: 'root',
+        class: ['red', 'hard'],
+      },
+      [
+        h('span', { class: 'blue' }, 'Hello World!'),
+        h('div', { class: 'red' }, 'test'),
+        'string',
+      ],
+    ),
 }
--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -5,7 +5,45 @@ export function render(vnode: any, container: any) {
 }
 
 export function patch(vnode: any, container: any) {
-  processComponent(vnode, container)
+  if (typeof vnode === 'string') {
+    processText(vnode, container)
+  } else if (typeof vnode.type === 'string') {
+    processElement(vnode, container)
+  } else {
+    processComponent(vnode, container)
+  }
+}
+
+function processText(vnode: any, container: any) {
+  const textContainer = document.createTextNode(vnode)
+  container.appendChild(textContainer)
+}
+
+function processElement(vnode: any, container: any) {
+  const el = document.createElement(vnode.type)
+
+  if (vnode.props) {
+    for (const key in vnode.props) {
+      const value = vnode.props[key]
+      el.setAttribute(key, value)
+    }
+  }
+
+  if (vnode.children) {
+    if (Array.isArray(vnode.children)) {
+      mountChildren(vnode, el)
+    } else {
+      patch(vnode.children, el)
+    }
+  }
+
+  container.appendChild(el)
+}
+
+function mountChildren(vnode, container: any) {
+  vnode.children.forEach((v) => {
+    patch(v, container)
+  })
 }
 
 function processComponent(vnode: any, container: any) {
